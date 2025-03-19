e3c79c6896e4d2c32f61e5b5c1c1566b5287424c
CormacOConnor72 <cormacoconnor72@outlook.ie>
2025-03-19 15:05:10 -0400

    formatted output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,6 +8,10 @@ def emotion_detector(text_to_analyse):
 
     response = requests.post(url,json = myobj, headers = header)
     
+    response_dict = json.loads(response.text)
+    emotion_data = response_dict['emotionPredictions'][0]['emotion']
 
-    return response.text 
+    emotion_data["dominant_emotion"] = max(emotion_data, key=lambda k: emotion_data[k])
+
+    return emotion_data
     
